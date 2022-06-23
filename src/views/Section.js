1671108deb469f179d22f5a01db37b2294526ab4
web.ljg <web.ljg@gmail.com>
2022-06-24 00:24:54 +0900

    Update: Presentational Component

--- src/views/Section.js
+++ src/views/Section.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+import ContentLogo from '../components/ContentLogo'
+import Metrics from '../components/Metrics'
+import Awards from '../components/Awards'
+
 const Container = styled.section`
   display: flex;
   justify-content: center;
@@ -7,7 +11,18 @@ const Container = styled.section`
   padding-top: 150px;
   width: 1200px;
 `
+const Contents = styled.div`
+  margin-left: 223px;
+`
 
-const Section = () => <Container />
+const Section = () => (
+  <Container>
+    <ContentLogo />
+    <Contents>
+      <Metrics />
+      <Awards />
+    </Contents>
+  </Container>
+)
 
 export default Section
