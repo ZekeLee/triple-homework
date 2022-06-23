26bbe7e204a2f9370e4be1afa696501c72ed0398
web.ljg <web.ljg@gmail.com>
2022-06-24 00:13:14 +0900

    Update: Component Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import GlobalStyle from './GlobalStyle'
+import styled from 'styled-components'
+
+import GlobalStyle from './Styles/GlobalStyle'
+import Section from './views/Section'
+import ContentLogo from './components/ContentLogo'
+import Metrics from './components/Metrics'
+import Awards from './components/Awards'
+
+const Contents = styled.div`
+  margin-left: 223px;
+`
 
 const App = () => (
   <>
     <GlobalStyle />
-    <section />
+    <Section>
+      <ContentLogo />
+      <Contents>
+        <Metrics />
+        <Awards />
+      </Contents>
+    </Section>
   </>
 )
 
