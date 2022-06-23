1e9b7e739b3f90a21e600b480467bf31ab32b5f1
web.ljg <web.ljg@gmail.com>
2022-06-24 00:13:48 +0900

    Add: Presentational Component

---
+++ src/views/Section.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  padding-top: 150px;
+  width: 1200px;
+`
+
+const Section = () => <Container />
+
+export default Section
