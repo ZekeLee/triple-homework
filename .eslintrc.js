3965a07f7553577a486a143e9f29205bd1a4d00d
web.ljg <web.ljg@gmail.com>
2022-06-24 13:44:31 +0900

    Fix: no-console rule 해제

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend' })
+const { extends: extendConfigs, overrides } = createConfig({ type: 'frontend' })
+
+module.exports = {
+  extends: [
+    ...extendConfigs,
+  ],
+  overrides: [
+    ...overrides,
+  ],
+  rules: {
+    rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  },
+}
