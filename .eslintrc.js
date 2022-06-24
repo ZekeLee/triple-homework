12691c413f52e2c607f0ee1a22704267e51b5f7e
web.ljg <web.ljg@gmail.com>
2022-06-24 22:19:30 +0900

    Update: TypeScript 관련 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-const { extends: extendConfigs, overrides } = createConfig({ type: 'frontend' })
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
 
 module.exports = {
   extends: [...extendConfigs],
